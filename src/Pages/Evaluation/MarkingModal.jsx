cc37aeafece353716458eb7f62043496b035b78a
jangbi96 <84945932+jangbi96@users.noreply.github.com>
2022-09-16 15:53:17 +0900

    ㅁ

---
+++ src/Pages/Evaluation/MarkingModal.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function MarkingModal() {
+    return (
+        <div className="modal">
+            <div className="dim"></div>
+            <div className='markingModal cmmnModal'>
+                <div className="markingModal-head cmmnModal-body">
+
+                </div>
+                <div className="markingModal-body cmmnModal-body">
+
+        
+                </div>
+                <div className="markingModal-foot cmmnModal-body">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MarkingModal;
