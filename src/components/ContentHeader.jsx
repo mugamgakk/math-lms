3b37e730b0f075bd88c7917bcc5ebbec5e087702
mugamgakk <88496769+mugamgakk@users.noreply.github.com>
2022-10-11 14:03:59 +0900

    content header

--- src/components/ContentHeader.jsx
+++ src/components/ContentHeader.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 
-function ContentHeader({title}) {
+function ContentHeader({title, location}) {
     return ( 
         <header className='content-header'>
             <h1 className='title'>{title}</h1>
-            <ul className='location-list'>
-                <li>마이페이지 &gt; </li>
-                <li>수학 학습 관리 &gt; </li>
-                <li>{title}  </li>
-            </ul>
+            <strong>
+                {location}
+            </strong>
         </header>
      );
 }
