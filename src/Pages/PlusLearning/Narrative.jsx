94bf0b7a89301021d1f432af7c5315cd43f507bb
mugamgakk <88496769+mugamgakk@users.noreply.github.com>
2022-11-08 14:27:44 +0900

    ㅁㄴㅇ

--- src/Pages/PlusLearning/Narrative.jsx
+++ src/Pages/PlusLearning/Narrative.jsx
@@ -57,7 +57,6 @@ const DATA = [
 function Narrative() {
     const clickStudent = useStudentsStore((state) => state.clickStudent);
     const bookList = useStudentsStore((state) => state.bookList);
-    console.log("선택된 북리스트",bookList)
     // tr data
     let [plusData, setPlusData] = useState([]);
     // 단원
