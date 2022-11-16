f452ff0cc475580cb8fc64db4b2f1793d4eabee4
mugamgakk <88496769+mugamgakk@users.noreply.github.com>
2022-11-16 15:59:55 +0900

    checkbox to component

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const guidGenerator = ()=>{
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+function Checkbox({ color = "green", id = guidGenerator(), checked, onChange, disabled }) {
+    return (
+        <div className={"check-" + color}>
+            <input
+                type="checkbox"
+                id={id}
+                checked={checked}
+                onChange={(e) => {
+                    onChange && onChange(e);
+                }}
+                disabled={disabled}
+            />
+            <label htmlFor={id}></label>
+        </div>
+    );
+}
+
+export default Checkbox;
