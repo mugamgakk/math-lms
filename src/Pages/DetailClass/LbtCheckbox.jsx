65ba3e14c392db9d060ea7e271f59dd79e7af5d0
mugamgakk <88496769+mugamgakk@users.noreply.github.com>
2022-11-30 14:08:42 +0900

    학생 분석표 commit

--- src/Pages/DetailClass/LbtCheckbox.jsx
+++ src/Pages/DetailClass/LbtCheckbox.jsx
@@ -1,79 +1,25 @@
-import React from "react";
-import { useRef } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import useLbtStore from "../../store/useLbtStore";
-import style from "../../style/style-module/lbtModal.module.scss";
+import React from 'react';
+import Checkbox from '../../components/Checkbox';
 
-function LbtCheckbox({ checkbox, count, allCheckBtn }) {
-    let [checkList, setCheckList] = useState(checkbox.optionItem);
-    let setCreateData = useLbtStore((state) => state.setCreateData);
-
-    let ref = useRef(false);
-
-    const allCheck = (checked) => {
-        if (checked) {
-            setCheckList(checkbox.optionItem);
-        } else {
-            setCheckList([]);
-        }
-    };
-
-    const oneCheck = (checked, ele) => {
-        if (checked) {
-            setCheckList([...checkList, ele]);
-        } else {
-            setCheckList(checkList.filter((a) => a !== ele));
-        }
-    };
-
-    useEffect(() => {
-        setCreateData({ option: checkbox.option, optionItem: checkList });
-    }, [count]);
-
-    useEffect(() => {
-        if (ref.current) {
-            if (allCheckBtn) {
-                setCheckList(checkbox.optionItem);
-            } else {
-                setCheckList([]);
-            }
-        } else {
-            ref.current = true;
-        }
-    }, [allCheckBtn]);
-
+function LbtCheckbox({ ele }) {
     return (
-        <div>
-            <div>
-                <label htmlFor={checkbox.option}>{checkbox.option}</label>
-                <input
-                    type="checkbox"
-                    id={checkbox.option}
-                    onChange={(e) => {
-                        allCheck(e.target.checked);
-                    }}
-                    checked={checkList.length === checkbox.optionItem.length}
-                    className={style.formConrol}
-                />
+        <div className="part" key={ele.option}>
+            <div className="part-title">
+                <Checkbox color="orange" id={ele.option} />
+                <label htmlFor={ele.option}>{ele.option}</label>
             </div>
-            <ul className={style.contentList} style={{ marginLeft: "20px" }}>
-                {checkbox.optionItem.map((a) => {
-                    return (
-                        <li key={a}>
-                            <label htmlFor={a}>{a}</label>
-                            <input
-                                type="checkbox"
-                                id={a}
-                                className={style.formConrol}
-                                checked={checkList.includes(a)}
-                                onChange={(e) => {
-                                    oneCheck(e.target.checked, a);
-                                }}
-                            />
-                        </li>
-                    );
-                })}
+            <ul className="part-list">
+                {
+                    ele.optionItem.map(a => {
+                        return (
+                            <li>
+                                <Checkbox color="orange" id={a} />
+                                <label htmlFor={a}>{a}</label>
+                            </li>
+                        )
+                    })
+                }
+
             </ul>
         </div>
     );
--- src/Pages/DetailClass/modal/LbtModal.jsx
+++ src/Pages/DetailClass/modal/LbtModal.jsx
@@ -3,6 +3,9 @@ import style from "../../../style/style-module/lbtModal.module.scss";
 import LbtCheckbox from "../LbtCheckbox";
 import CreateLbt from "../CreateLbt/CreateLbt";
 import useLbtStore from "../../../store/useLbtStore";
+import { falseModal } from "../../../methods/methods";
+import Icon from "../../../components/Icon";
+import Checkbox from "../../../components/Checkbox";
 
 const dataLists = [
     {
@@ -33,86 +36,161 @@ const dataLists = [
 ];
 
 
-function LbtModal({setCreateModal }) {
+function LbtModal({ setCreateModal }) {
     const [allCheckBtn, setAllCheckBtn] = useState(false);
     const printComponent = useRef();
+
     // 적용을 눌렀을때 데이터를 가져오기위한
     const [count, setCount] = useState(0);
-    const createData = useLbtStore(state=> state.createData);
+    const createData = useLbtStore(state => state.createData);
+
+    const allCheck = (checked) => {
+        setAllCheckBtn(!allCheckBtn);
+    }
 
     return (
-        <div
-            className={style.modal}
-            onClick={(e) => {
-                if (e.target === e.currentTarget) {
-                    setCreateModal(false)
-                }
-            }}
-        >
-            <div className={style.content}>
-                <h4>종합 학습 분석표 생성하기</h4>
-                <div className="row">
-                    <div className={style.left}>
-
-                        <strong>
-                            평균 표시(
-                            <input
-                                type="checkbox"
-                                className={style.formConrol}
-                                defaultChecked
-                                disabled
-                            />
-                            Y
-                            <input type="checkbox" className={style.formConrol} disabled />
-                            N)
+        <div className="modal LbtModal" onClick={(e) => { falseModal(e, setCreateModal) }}>
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h4 className="modal-title">분석표 생성</h4>
+                    <button className='btn'>
+                        <Icon icon={"close"} />
+                    </button>
+                </div>
+                <div className="modal-body">
+                    <div className="modal-name">
+                        <strong className="name">
+                            종합 학습 분석표 생성하기
                         </strong>
-
-                        <div>
-                            <button
-                                className="btn"
-                                onClick={()=>{
-                                    setAllCheckBtn(!allCheckBtn)
-                                }}
-                            >
-                                모두 선택 / 해제
-                            </button>
-                        </div>
-
-                        <div className="contentGroup">
-                            {dataLists.map((checkbox, i) => {
-                                return (
-                                    <LbtCheckbox key={i} checkbox={checkbox} count={count} allCheckBtn={allCheckBtn} />
-                                );
-                            })}
-                        </div>
-
-                        <button
-                            className="btn"
-                            onClick={()=>{setCount(count + 1)}}
-                        >
-                            적용
-                        </button>
                     </div>
-                    <div className={style.rightWrap}>
-                        <CreateLbt printComponent={printComponent} />
+                    <div className="LbtModal-body">
+                        <div className="left">
+                            <div className="head">
+                                ※ 평균 표시
+                            </div>
+                            <div className="body">
+                                <div className="part">
+                                    <div className="part-title">
+                                        <Checkbox color="orange" id="all" onChange={(e) => { allCheck(e.target.checked) }} />
+                                        <label htmlFor="all">모두 선택 / 해제</label>
+                                    </div>
+                                </div>
+                                <div className="part">
+                                    <div className="part-title">
+                                        <Checkbox color="orange" id="a" />
+                                        <label htmlFor="a">교재 학습 분석</label>
+                                    </div>
+                                    <ul className="part-list">
+                                        <li>
+                                            <Checkbox color="orange" id="b" />
+                                            <label htmlFor="b">교재 학습 분석</label>
+                                        </li>
+                                        <li>
+                                            <Checkbox color="orange" id="c" />
+                                            <label htmlFor="c">교재 학습 분석</label>
+                                        </li>
+                                    </ul>
+                                </div>
+                                {
+                                    dataLists.map(a => {
+                                        return (
+                                            <LbtCheckbox ele={a} />
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div className="right">
+                            <div className="head">
+                                <span style={{ marginRight: "10px" }}>
+                                    사유하고 질문하라.
+                                </span>
+                                Parallax Thingking!
+                            </div>
+                            <div className="body">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className={style.btnGroup}>
-                    <button className="btn" onClick={()=>{
-                        createData();
-                        setCreateModal(false)
-                    }}>생성</button>
-                    <button
-                        className="btn"
-                        onClick={() => {
-                            setCreateModal(false)
-                        }}
-                    >
-                        닫기
-                    </button>
+                <div className="modal-footer">
+                    <button className="btn-grey-border" style={{ marginRight: "5px" }}>닫기</button>
+                    <button className="btn-orange">생성하기</button>
                 </div>
             </div>
         </div>
+        // <div
+        //     className={style.modal}
+        //     onClick={(e) => {
+        //         if (e.target === e.currentTarget) {
+        //             setCreateModal(false)
+        //         }
+        //     }}
+        // >
+        //     <div className={style.content}>
+        //         <h4>종합 학습 분석표 생성하기</h4>
+        //         <div className="row">
+        //             <div className={style.left}>
+
+        //                 <strong>
+        //                     평균 표시(
+        //                     <input
+        //                         type="checkbox"
+        //                         className={style.formConrol}
+        //                         defaultChecked
+        //                         disabled
+        //                     />
+        //                     Y
+        //                     <input type="checkbox" className={style.formConrol} disabled />
+        //                     N)
+        //                 </strong>
+
+        //                 <div>
+        //                     <button
+        //                         className="btn"
+        //                         onClick={()=>{
+        //                             setAllCheckBtn(!allCheckBtn)
+        //                         }}
+        //                     >
+        //                         모두 선택 / 해제
+        //                     </button>
+        //                 </div>
+
+        //                 <div className="contentGroup">
+        //                     {dataLists.map((checkbox, i) => {
+        //                         return (
+        //                             <LbtCheckbox key={i} checkbox={checkbox} count={count} allCheckBtn={allCheckBtn} />
+        //                         );
+        //                     })}
+        //                 </div>
+
+        //                 <button
+        //                     className="btn"
+        //                     onClick={()=>{setCount(count + 1)}}
+        //                 >
+        //                     적용
+        //                 </button>
+        //             </div>
+        //             <div className={style.rightWrap}>
+        //                 <CreateLbt printComponent={printComponent} />
+        //             </div>
+        //         </div>
+        //         <div className={style.btnGroup}>
+        //             <button className="btn" onClick={()=>{
+        //                 createData();
+        //                 setCreateModal(false)
+        //             }}>생성</button>
+        //             <button
+        //                 className="btn"
+        //                 onClick={() => {
+        //                     setCreateModal(false)
+        //                 }}
+        //             >
+        //                 닫기
+        //             </button>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
