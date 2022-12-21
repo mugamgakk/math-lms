4b412ba2fa621a880c2a8320c991bd5b5359e40c
mugamgakk <88496769+mugamgakk@users.noreply.github.com>
2022-12-21 14:57:50 +0900

    파일 사이즈 알기

--- src/methods/methods.js
+++ src/methods/methods.js
@@ -3,17 +3,16 @@ import JSZip from "jszip";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
-export function toggleBodyScroll(param){
-    if(param === true){
+export function toggleBodyScroll(param) {
+    if (param === true) {
         document.body.style.overflow = "hidden";
-    }else{
+    } else {
         document.body.style.overflow = "unset";
     }
 }
 
-export function falseModal(e,target){
-    
-    if(e.target === e.currentTarget){
+export function falseModal(e, target) {
+    if (e.target === e.currentTarget) {
         var 변경 = target;
         변경(false);
     }
@@ -22,19 +21,19 @@ export function falseModal(e,target){
 function weekChange(param) {
     switch (param) {
         case 0:
-            return "일"
+            return "일";
         case 1:
-            return "월"
+            return "월";
         case 2:
-            return "화"
+            return "화";
         case 3:
-            return "수"
+            return "수";
         case 4:
-            return "목"
+            return "목";
         case 5:
-            return "금"
+            return "금";
         case 6:
-            return "토"
+            return "토";
     }
 }
 
@@ -46,12 +45,11 @@ const getByteSize = (size) => {
     const byteUnits = ["kb", "mb"];
 
     for (let i = 0; i < byteUnits.length; i++) {
-        size = (size / 1024);
+        size = size / 1024;
 
         if (size < 1024) return size.toFixed(1) + byteUnits[i];
     }
 };
-
 
 //  파일 한개를 파라미터로 넣어서 다운로드한다.
 //  두번째 파라미터로 파일명을 지정한다 (기본값 : 원본 파일명)
@@ -63,131 +61,137 @@ const fileDown = (file, fileName = file.originalFileName) => {
     // FileSaver.saveAs(blob, fileName)
     FileSaver.saveAs(file, fileName);
     // FileSaver.saveAs("https://httpbin.org/image", "image.jpg");
-}
+};
 
 // zip 만들어주는 함수
 // 파라미터로 파일들이 들어있는 배열을 넣는다
 // 두번째 파라미터로 파일명을 지정한다 (기본값 : download)
 const makeZip = (filesArr, fileName = "download") => {
-
-    if(filesArr.length === 0){
-        return 
+    if (filesArr.length === 0) {
+        return;
     }
 
     const zip = new JSZip();
 
-    filesArr.forEach(ele => {
-        zip.file(ele.name, ele, {binary: true})
-    })
+    filesArr.forEach((ele) => {
+        zip.file(ele.name, ele, { binary: true });
+    });
 
-    zip.generateAsync({type: "blob"})
-        .then(content => {
-            FileSaver.saveAs(content, fileName)
-        })
-}
+    zip.generateAsync({ type: "blob" }).then((content) => {
+        FileSaver.saveAs(content, fileName);
+    });
+};
 
 // 가나다 순 정렬
 // 1. 첫번째 파라미터 배열넣기
 // 2. 두번째 파라미터 배열이 오브젝트일시 key 값을 넣음
-const arrSort = (arr, key = undefined, order = -1)=>{
+const arrSort = (arr, key = undefined, order = -1) => {
     let copy = [...arr];
 
-    if(typeof copy[0] === "object"){
-        copy.sort((a,b)=>{
-            if(a[key] < b[key]){
-                return order === -1 ? -1 : 1
-            }else{
-                return order === -1 ? 1 : -1
+    if (typeof copy[0] === "object") {
+        copy.sort((a, b) => {
+            if (a[key] < b[key]) {
+                return order === -1 ? -1 : 1;
+            } else {
+                return order === -1 ? 1 : -1;
             }
-        })
-    }else{
-        copy.sort((a,b)=>{
-            if(a < b){
-                return order === -1 ? -1 : 1
-            }else{
-                return order === -1 ? 1 : -1
+        });
+    } else {
+        copy.sort((a, b) => {
+            if (a < b) {
+                return order === -1 ? -1 : 1;
+            } else {
+                return order === -1 ? 1 : -1;
             }
-        })
+        });
     }
 
-    return copy
-}
+    return copy;
+};
 
 // 숫자에 콤마 넣어주는 함수
-const comma = (num)=>{
+const comma = (num) => {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+};
+
+export const getUrlFileSize = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.blob();
+        const ext = url.split(".").pop();
+        const filename = url.split("/").pop();
+        const metadata = { type: `image/${ext}` };
+        const size = new File([data], filename, metadata).size;
+        return size;
+    } catch (err) {
+        console.log("cors 에러");
+        return 0
+    }
+};
 
 // jpg다운로드 (display none 일때는 안됨)
-export function htmlToImg (target, filName = "download", format = "jpeg"){
-      html2canvas(target).then((canvas) => {
-            const imgFile = canvas.toDataURL("image/" + format);
-            // png로 바꾸면 png로 됨
-            const imgName = filName;
+export function htmlToImg(target, filName = "download", format = "jpeg") {
+    html2canvas(target).then((canvas) => {
+        const imgFile = canvas.toDataURL("image/" + format);
+        // png로 바꾸면 png로 됨
+        const imgName = filName;
 
-            FileSaver.saveAs(imgFile, imgName);
-        });
+        FileSaver.saveAs(imgFile, imgName);
+    });
 }
 
-export function _map(list, fn){
-    const arr = [];
-    
-    _each(list, function(ele, i){
-        arr.push(fn(ele, i))
-    })
-
-    return arr
-}
-
-export function _filter(list, fn){
+export function _map(list, fn) {
     const arr = [];
 
-    _each(list, function(ele, i){
-        if(fn(ele, i)){
-            arr.push(ele)
-        }
-    })
+    _each(list, function (ele, i) {
+        arr.push(fn(ele, i));
+    });
 
     return arr;
 }
 
-export function _each(list, fn){
-    for(var i = 0; i < list.length; i++){
-        fn(list[i], i)
-    }
+export function _filter(list, fn) {
+    const arr = [];
 
-    return list
+    _each(list, function (ele, i) {
+        if (fn(ele, i)) {
+            arr.push(ele);
+        }
+    });
+
+    return arr;
 }
 
-export function _remove(list, obj){
+export function _each(list, fn) {
+    for (var i = 0; i < list.length; i++) {
+        fn(list[i], i);
+    }
+
+    return list;
+}
+
+export function _remove(list, obj) {
     var key = Object.keys(obj)[0];
     var value = Object.values(obj)[0];
 
     var arr = [];
-    for(let i = 0; i < list.length; i++){
-        if(list[i][key] !== value){
-            arr.push(list[i])
+    for (let i = 0; i < list.length; i++) {
+        if (list[i][key] !== value) {
+            arr.push(list[i]);
         }
-    }   
+    }
 
     return arr;
 }
 
-export function _cloneDeep(list){
+export function _cloneDeep(list) {
     let arr = [];
 
-    list.forEach(a=>{
-        arr.push({...a})
-    })
+    list.forEach((a) => {
+        arr.push({ ...a });
+    });
 
-    return arr
+    return arr;
 }
 
-export {
-    weekChange,
-    getByteSize,
-    fileDown,
-    makeZip,
-    arrSort,
-    comma
-}
+export { weekChange, getByteSize, fileDown, makeZip, arrSort, comma };
